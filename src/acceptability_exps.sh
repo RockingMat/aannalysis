d9fa8b92ed17ea74f3155972341a3ab84de1074f
Kanishka Misra <km55359@phyl-ling-p01.la.utexas.edu>
2023-11-14 13:41:20 -0600

    no determiner ANN results

--- src/acceptability_exps.sh
+++ src/acceptability_exps.sh
@@ -11,11 +11,13 @@ declare -a modes=(aann anan naan)
 for mode in ${modes[@]}
 do 
 
-    python src/acceptability.py -m kanishka/smolm-autoreg-bpe-babylm-aann-counterfactual-anan-1e-3 -b 128 -a data/mahowald-${mode}
+    # python src/acceptability.py -m kanishka/smolm-autoreg-bpe-babylm-aann-counterfactual-anan-1e-3 -b 128 -a data/mahowald-${mode}
 
-    python src/acceptability.py -m kanishka/smolm-autoreg-bpe-babylm-aann-counterfactual-naan-3e-3 -b 128 -a data/mahowald-${mode}
+    # python src/acceptability.py -m kanishka/smolm-autoreg-bpe-babylm-aann-counterfactual-naan-3e-3 -b 128 -a data/mahowald-${mode}
 
-    python src/acceptability.py -m kanishka/smolm-autoreg-bpe-babylm-no_aann-infilling-1e-3 -b 128 -a data/mahowald-${mode}
+    # python src/acceptability.py -m kanishka/smolm-autoreg-bpe-babylm-no_aann-infilling-1e-3 -b 128 -a data/mahowald-${mode}
 
-    python src/acceptability.py -m kanishka/smolm-autoreg-bpe-babylm-no_aann-removal-3e-3 -b 128 -a data/mahowald-${mode}
+    # python src/acceptability.py -m kanishka/smolm-autoreg-bpe-babylm-no_aann-removal-3e-3 -b 128 -a data/mahowald-${mode}
+
+    python src/acceptability.py -m kanishka/smolm-autoreg-bpe-babylm-no_aann-all-det-removal-1e-3 -b 128 -a data/mahowald-${mode}
 done
