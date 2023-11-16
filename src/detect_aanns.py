25366603f97c02994999642cd55e96b77c6e20fe
Kanishka Misra <km55359@phyl-ling-p01.la.utexas.edu>
2023-11-15 21:35:00 -0600

    script to run aann detector over babylm sents

---
+++ src/detect_aanns.py
@@ -0,0 +1,31 @@
+from minicons import supervised
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+detector = supervised.SupervisedHead("kanishka/aann-detector", device="cuda:2")
+
+# print(lm.logits(["the family spent a beautiful five days in austin.", "the boys spent an eventful few days in the city."], probs=False).argmax(1))
+
+sents = []
+with open(
+    "/home/km55359/rawdata/babylm_data/babylm_100M/sents/babylm_sents.txt", "r"
+) as f:
+    for line in f:
+        sents.append(line.strip())
+
+dl = DataLoader(sents, batch_size=128, shuffle=False, num_workers=0)
+
+labels = []
+for batch in tqdm(dl):
+    labels.extend(detector.logits(batch, probs=False).argmax(1).tolist())
+
+aanns = []
+for i, label in enumerate(labels):
+    if label == 1:
+        aanns.append(sents[i])
+
+print("Number of AANNs found using the classifier: ", len(aanns))
+
+with open("data/babylm-aanalysis/detected_aann_sents.txt", "w") as f:
+    for aann in aanns:
+        f.write(aann + "\n")
