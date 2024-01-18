5d72a01b3275b00da2904b1e457384d0374dac23
Kanishka Misra <menogetusername@gmail.com>
2024-01-18 13:34:22 -0600

    measure nouns fix

--- src/acceptability_exps.sh
+++ src/acceptability_exps.sh
@@ -9,8 +9,6 @@
 declare -a modes=(aann anan naan)
 declare -a lrs=(1e-3 1e-4 3e-4)
 
-# declare -a models=$(awk -F "\"*,\"*" '{print $2}'| tail -n +2)
-
 readarray -t models < <( awk -F "\"*,\"*" '{print $2}' data/results/babylm_lms.csv | tail -n +2 ); IFS=' '
 
 for mode in ${modes[@]}
@@ -27,15 +25,20 @@ do
             python src/acceptability.py -m kanishka/smolm-autoreg-bpe-${model}-seed_211-${lr} -b 128 -a data/mahowald-${mode}
         done
     done
-
-    # python src/acceptability.py -m kanishka/smolm-autoreg-bpe-babylm-aann-counterfactual-anan-1e-3 -b 128 -a data/mahowald-${mode}
-
-    # python src/acceptability.py -m kanishka/smolm-autoreg-bpe-babylm-aann-counterfactual-naan-3e-3 -b 128 -a data/mahowald-${mode}
-
-    # python src/acceptability.py -m kanishka/smolm-autoreg-bpe-babylm-no_aann-infilling-1e-3 -b 128 -a data/mahowald-${mode}
-
-    # python src/acceptability.py -m kanishka/smolm-autoreg-bpe-babylm-no_aann-removal-3e-3 -b 128 -a data/mahowald-${mode}
-
-    # python src/acceptability.py -m kanishka/smolm-autoreg-bpe-babylm-no_aann-all-det-removal-1e-3 -b 128 -a data/mahowald-${mode}
-
 done
+
+
+# for mode in ${modes[@]}
+# do 
+#     for lr in ${lrs[@]}
+#     do
+#         model='counterfactual-babylm-measure_nouns_as_singular'
+#             # echo kanishka/smolm-autoreg-bpe-${model}-${lr} -b 128 -a data/mahowald-${mode}
+#         python src/acceptability.py -m kanishka/smolm-autoreg-bpe-${model}-${lr} -b 128 -a data/mahowald-${mode}
+
+#         python src/acceptability.py -m kanishka/smolm-autoreg-bpe-${model}-seed_1024-${lr} -b 128 -a data/mahowald-${mode}
+
+#         python src/acceptability.py -m kanishka/smolm-autoreg-bpe-${model}-seed_211-${lr} -b 128 -a data/mahowald-${mode}
+#     done
+
+# done
