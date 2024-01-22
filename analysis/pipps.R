104c0e455b5ad599ad4f67d6edab0d8ccc919380
Kanishka Misra <menogetusername@gmail.com>
2024-01-22 14:56:37 -0600

    so many bugs

--- analysis/pipps.R
+++ analysis/pipps.R
@@ -2,6 +2,8 @@ library(tidyverse)
 
 pipps_raw <- read_csv("data/pipps/materials.jsonl")
 
+potts_raw <- read_csv("~/projects/pipps/results/potts_pipps_results.csv")
+
 pipps_results <- fs::dir_ls("data/results/pipps/") %>%
   keep(str_detect(., "csv")) %>%
   map_df(read_csv, .id = "model") %>%
@@ -9,6 +11,14 @@ pipps_results <- fs::dir_ls("data/results/pipps/") %>%
     model = str_remove(model, "data/results/pipps/"),
     model = str_remove(model, ".csv")
   )
+
+pipps_long <- pipps_results %>%
+  select(-idx) %>%
+  pivot_longer(pipp_filler_gap:no_filler_gap, names_to = "condition", values_to = "target_surprisal")
+
+pipps_long %>%
+  inner_join(potts_raw %>% rename(potts_surprisal = target_surprisal)) %>%
+  mutate(diff = target_surprisal - potts_surprisal)
 
 pipps_results %>%
   group_by(model, preposition, embedding) %>%
