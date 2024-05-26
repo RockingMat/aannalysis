e6fc582f70e55a5a962332ce9e63b4babc66cfba
Kanishka Misra <menogetusername@gmail.com>
2024-05-26 15:03:40 -0500

    unigrams for new regex

--- scripts/slor_exps.sh
+++ scripts/slor_exps.sh
@@ -2,19 +2,21 @@
 
 # readarray -t models < <( ls models/unigrams/ ); IFS=' '
 
-# for model in ${models[@]}
-# do 
-#     python src/ngram_tokenized_acceptability.py -m models/unigrams/${model} --unigram
-# done
+declare -a models=(counterfactual-babylm-old_union_new_regex_aanns_removal.csv counterfactual-babylm-new_regex_aanns_removal.csv)
+
+for model in ${models[@]}
+do 
+    python src/ngram_tokenized_acceptability.py -m models/unigrams/${model} --ngram 1
+done
 
 
-readarray -t models < <( ls models/fourgrams/ ); IFS=' '
+# readarray -t models < <( ls models/fourgrams/ ); IFS=' '
 # declare -a models=(counterfactual-babylm-measure-nouns-as-singular_removal.binary)
 # declare -a models=(counterfactual-babylm-only_random_removal.binary)
 # declare -a models=(counterfactual-babylm-only_indef_articles_with_pl_nouns_removal counterfactual-babylm-only_other_det_removal counterfactual-babylm-only_random_removal counterfactual-babylm-only_measure_nps_as_singular_removal)
 
-for model in ${models[@]}
-do 
-    # python src/ngram_tokenized_acceptability.py -m models/bigrams/${model} --ngram 2
-    python src/ngram_tokenized_acceptability.py -m models/fourgrams/${model} --ngram 4
-done
+# for model in ${models[@]}
+# do 
+#     # python src/ngram_tokenized_acceptability.py -m models/bigrams/${model} --ngram 2
+#     python src/ngram_tokenized_acceptability.py -m models/fourgrams/${model} --ngram 4
+# done
