130ec15f02a48a3af1d650e33f8c49184ba30cd1
Kanishka Misra <menogetusername@gmail.com>
2024-07-03 19:35:37 -0500

    decomposed aanns and dt aanns with new regex

--- src/generate_counterfactual_corpora.sh
+++ src/generate_counterfactual_corpora.sh
@@ -139,5 +139,24 @@
 #     --aann_path data/babylm-analysis/new_regex_aanns.csv \
 #     --counterfactual_type removal 
 
+# python src/counterfactual_constructions.py \
+#     --output_path data/training_data/counterfactual_babylm_measure_nps_as_singular_new.txt \
+#     --aann_path data/babylm-analysis/new_regex_aanns.csv \
+#     --counterfactual_type removal \
+#     --excess_path data/babylm-analysis/measure_nouns_with_singular_verbs.csv \
+#     --secondary_excess_path data/babylm-analysis/indef_articles_with_pl_nouns.csv
+
+# python src/counterfactual_constructions.py \
+#     --output_path data/training_data/counterfactual_babylm_aann_indef_articles_with_pl_nouns_removal_new.txt \
+#     --aann_path data/babylm-analysis/new_regex_aanns.csv \
+#     --counterfactual_type removal \
+#     --excess_path data/babylm-analysis/indef_articles_with_pl_nouns.csv
+
+python src/counterfactual_constructions.py \
+    --output_path data/training_data/counterfactual_babylm_aann_dtanns.txt \
+    --aann_path data/babylm-analysis/new_regex_aanns.csv \
+    --counterfactual_type removal \
+    --excess_path data/babylm-analysis/dtanns.csv
+
 
 
