76f3a87e8059208a8cab10495fe3ed500200da48
Kanishka Misra <menogetusername@gmail.com>
2024-07-20 09:24:10 -0500

    variability logprob results

--- src/acceptability_exps.sh
+++ src/acceptability_exps.sh
@@ -7,11 +7,44 @@
 # python src/acceptability.py -m kanishka/smolm-autoreg-bpe-babylm-aann-counterfactual-naan-3e-3 -b 128 -a data/mahowald-naan
 
 # declare -a modes=(aann anan naan)
-declare -a modes=(aann)
-# declare -a lrs=(1e-3 1e-4 3e-4)
-declare -a lrs=(1e-4)
+# declare -a modes=(aann)
+# # declare -a lrs=(1e-3 1e-4 3e-4)
+# declare -a lrs=(1e-4)
 
-# readarray -t models < <( awk -F "\"*,\"*" '{print $2}' data/results/babylm_lms.csv | tail -n +2 ); IFS=' '
+# # readarray -t models < <( awk -F "\"*,\"*" '{print $2}' data/results/babylm_lms.csv | tail -n +2 ); IFS=' '
+
+# # for mode in ${modes[@]}
+# # do 
+# #     for lr in ${lrs[@]}
+# #     do
+# #         for model in ${models[@]}
+# #         do
+# #             # echo kanishka/smolm-autoreg-bpe-${model}-${lr} -b 128 -a data/mahowald-${mode}
+# #             python src/acceptability.py -m kanishka/smolm-autoreg-bpe-${model}-${lr} -b 128 -a data/mahowald-${mode}
+
+# #             python src/acceptability.py -m kanishka/smolm-autoreg-bpe-${model}-seed_1024-${lr} -b 128 -a data/mahowald-${mode}
+
+# #             python src/acceptability.py -m kanishka/smolm-autoreg-bpe-${model}-seed_211-${lr} -b 128 -a data/mahowald-${mode}
+# #         done
+# #     done
+# # done
+
+# # declare -a models=(counterfactual-babylm-measure_nouns_as_singular)
+# # declare -a models=(counterfactual-babylm-aann-prototypical_only counterfactual-babylm-aann-no_prototypical)
+# # declare -a models=(counterfactual-babylm-random_removal)
+# # declare -a models=(counterfactual-babylm-only_other_det_removal counterfactual-babylm-only_indef_articles_with_pl_nouns_removal counterfactual-babylm-only_measure_nps_as_singular_removal)
+# # declare -a models=(counterfactual-babylm-only_random_removal)
+# # declare -a models=(meta-llama/Llama-2-7b-hf)
+# # declare -a models=(counterfactual-babylm-old_union_new_regex_aanns_removal counterfactual-babylm-new_regex_aanns_removal)
+
+# # declare -a models=(counterfactual_babylm_naans_new counterfactual_babylm_300_naans_new counterfactual_babylm_300_anans_new)
+# # declare -a models=(counterfactual_babylm_anans_new counterfactual_babylm_naans_new)
+# declare -a models=(counterfactual_babylm_measure_nps_as_singular_new)
+# declare -a lrs=(1e-3)
+# # declare -a models=(counterfactual_babylm_naans_new-1e-3 counterfactual_babylm_300_naans_new-1e-3 counterfactual_babylm_300_anans_new-1e-3 counterfactual_babylm_anans_new-1e-3)
+
+# declare -a models=(counterfactual_babylm_aann_dtanns)
+# declare -a lrs=(1e-4)
 
 # for mode in ${modes[@]}
 # do 
@@ -20,48 +53,15 @@ declare -a lrs=(1e-4)
 #         for model in ${models[@]}
 #         do
 #             # echo kanishka/smolm-autoreg-bpe-${model}-${lr} -b 128 -a data/mahowald-${mode}
+#             # python src/acceptability.py -m kanishka/smolm-autoreg-bpe-${model}-${lr} -b 128 -a data/mahowald-${mode} --device cuda:2
 #             python src/acceptability.py -m kanishka/smolm-autoreg-bpe-${model}-${lr} -b 128 -a data/mahowald-${mode}
 
-#             python src/acceptability.py -m kanishka/smolm-autoreg-bpe-${model}-seed_1024-${lr} -b 128 -a data/mahowald-${mode}
+#             # python src/acceptability.py -m kanishka/smolm-autoreg-bpe-${model}-seed_1024-${lr} -b 128 -a data/mahowald-${mode}
 
-#             python src/acceptability.py -m kanishka/smolm-autoreg-bpe-${model}-seed_211-${lr} -b 128 -a data/mahowald-${mode}
+#             # python src/acceptability.py -m kanishka/smolm-autoreg-bpe-${model}-seed_211-${lr} -b 128 -a data/mahowald-${mode}
 #         done
 #     done
 # done
-
-# declare -a models=(counterfactual-babylm-measure_nouns_as_singular)
-# declare -a models=(counterfactual-babylm-aann-prototypical_only counterfactual-babylm-aann-no_prototypical)
-# declare -a models=(counterfactual-babylm-random_removal)
-# declare -a models=(counterfactual-babylm-only_other_det_removal counterfactual-babylm-only_indef_articles_with_pl_nouns_removal counterfactual-babylm-only_measure_nps_as_singular_removal)
-# declare -a models=(counterfactual-babylm-only_random_removal)
-# declare -a models=(meta-llama/Llama-2-7b-hf)
-# declare -a models=(counterfactual-babylm-old_union_new_regex_aanns_removal counterfactual-babylm-new_regex_aanns_removal)
-
-# declare -a models=(counterfactual_babylm_naans_new counterfactual_babylm_300_naans_new counterfactual_babylm_300_anans_new)
-# declare -a models=(counterfactual_babylm_anans_new counterfactual_babylm_naans_new)
-declare -a models=(counterfactual_babylm_measure_nps_as_singular_new)
-declare -a lrs=(1e-3)
-# declare -a models=(counterfactual_babylm_naans_new-1e-3 counterfactual_babylm_300_naans_new-1e-3 counterfactual_babylm_300_anans_new-1e-3 counterfactual_babylm_anans_new-1e-3)
-
-declare -a models=(counterfactual_babylm_aann_dtanns)
-declare -a lrs=(1e-4)
-
-for mode in ${modes[@]}
-do 
-    for lr in ${lrs[@]}
-    do
-        for model in ${models[@]}
-        do
-            # echo kanishka/smolm-autoreg-bpe-${model}-${lr} -b 128 -a data/mahowald-${mode}
-            # python src/acceptability.py -m kanishka/smolm-autoreg-bpe-${model}-${lr} -b 128 -a data/mahowald-${mode} --device cuda:2
-            python src/acceptability.py -m kanishka/smolm-autoreg-bpe-${model}-${lr} -b 128 -a data/mahowald-${mode}
-
-            # python src/acceptability.py -m kanishka/smolm-autoreg-bpe-${model}-seed_1024-${lr} -b 128 -a data/mahowald-${mode}
-
-            # python src/acceptability.py -m kanishka/smolm-autoreg-bpe-${model}-seed_211-${lr} -b 128 -a data/mahowald-${mode}
-        done
-    done
-done
 
 # for mode in ${modes[@]}
 # do 
@@ -71,3 +71,17 @@ done
 #         python src/acceptability.py -m ${model} -b 32 -a data/mahowald-${mode}
 #     done
 # done
+
+# declare -a variability=(high low)
+declare -a variability=(low)
+declare -a types=(all numeral adj noun)
+
+for variability in ${variability[@]}
+do 
+    for type in ${types[@]}
+    do
+        # python src/acceptability.py -m kanishka/smolm-autoreg-bpe-counterfactual_babylm_aann_${variability}_variability_${type}-1e-3 -b 128 -a data/mahowald-aann
+        python src/acceptability.py -m kanishka/smolm-autoreg-bpe-counterfactual_babylm_aann_${variability}_variability_${type}-seed_211-1e-3 -b 128 -a data/mahowald-aann
+        python src/acceptability.py -m kanishka/smolm-autoreg-bpe-counterfactual_babylm_aann_${variability}_variability_${type}-seed_1024-1e-3 -b 128 -a data/mahowald-aann
+    done
+done
